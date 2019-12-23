2135e77f16f5287ad8f423c4ae811d9678b55aed
Jelle Piepenbrock <jellepiepenbrock@gmail.com>
2019-12-23 14:30:35 +0100

    Data function updated

--- get_data.py
+++ get_data.py
@@ -30,95 +30,66 @@ batch_size = 128
 # # policy.load_state_dict(torch.load("weights_100episodes_reward_quick_victory"))
 # optimizer = torch.optim.Adam(vae.parameters(), lr=1e-3)
 # print('Loaded model.')
-#
-# # https://github.com/sksq96/pytorch-vae/blob/master/vae-cnn.ipynb <-- source for code.
-# def loss_fn(recon_x, x, mu, logvar):
-#     BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
-#
-#     # see Appendix B from VAE paper:
-#     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-#     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-#     KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
-#
-#     return BCE + KLD, BCE, KLD
-#
-# def select_action():
-#     return 3
+
 
 def select_action():
     return np.random.randint(low=0, high=3)
-# def compare(x):
-#     recon_x, _, _ = vae(x)
-#     return torch.cat([x / 255, recon_x])
 
-def main(episodes):
+
+def main(episodes, episode_length):
     env = Neurosmash.Environment(timescale=timescale, size=size, port=port, ip=ip) # This is the main environment.
     end, reward, state = env.reset()
-    ep_cnt = 0 
-    states = []
-<<<<<<< HEAD
-    file_cnt = 0 
-    for episode in range(episodes+1):
-=======
-    actions = []
-    dones = []
 
-    while len(states) < 20:
-        epstates = []
-        epactions = []
-        epdones = []
+    ep_cnt = 0
+    all_states = []
+    all_actions = []
+    all_dones = []
+    all_rewards = []
 
->>>>>>> e3e57ff9b11e7d3646d23d401e00da3b642fe92c
+    file_cnt = 0
+
+    while len(all_states) < episodes:
+        episode_states = []
+        episode_actions = []
+        episode_dones = []
+        episode_rewards = []
+
         end, reward, state = env.reset()  # Reset environment and record the starting state
         done = False
         ep_cnt += 1
-        # print(episode)
 
-        for time in range(50):
-            state = np.array(state)
+
+        for time in range(episode_length):
+
             action = select_action()
-            epactions.append(action)
+            episode_actions.append(action)
+
             # Step through environment using chosen action
             done, reward, state = env.step(action)
             state = torch.FloatTensor(state).reshape(size, size, 3)
             state = state.permute(2, 0, 1)
-<<<<<<< HEAD
 
+            episode_states.append(state)
+            episode_actions.append(action)
+            episode_dones.append(done)
+            episode_rewards.append(reward)
+            print(time)
             if done:
-                states.append(state)
-                print('Death')
-
-                if len(states) == 1000:
-                    break
-                
-
-        print('Storing files')
-        states = torch.stack(states)
-        torch.save(states, '../data/death_state_{}.pt'.format(file_cnt))
-        file_cnt += 1
-        states = []
-        break
-
-=======
-            # print(state.shape)
-            epstates.append(state)
-            epdones.append(done)
-            if done:
-                print(time)
                 break
 
-        print(torch.stack(epstates).shape)
-        print(epactions[:20])
-        if len(epstates) >= 20:
-            states.append(torch.stack(epstates)[-20:])
-            actions.append(torch.Tensor(epactions[-20:]))
-            dones.append(torch.Tensor(epdones[-20:]))
 
+        if len(episode_states) >= 20:
+            all_states.append(torch.stack(episode_states)[-20:])
+            all_actions.append(torch.Tensor(episode_actions[-20:]))
+            all_dones.append(torch.Tensor(episode_dones[-20:]))
+            all_rewards.append(torch.Tensor(episode_rewards[-20:]))
 
-    torch.save(torch.stack(states), 'training_data.pt')
-    torch.save(torch.stack(actions), 'training_actions.pt')
-    torch.save(torch.stack(dones), 'training_dones.pt')
+    print(episode_actions)
+    print(episode_dones)
+    print(episode_rewards)
+    torch.save(torch.stack(all_states), 'training_data.pt')
+    torch.save(torch.stack(all_actions), 'training_actions.pt')
+    torch.save(torch.stack(all_dones), 'training_dones.pt')
+    torch.save(torch.stack(all_rewards), 'training_rewards.pt')
 
->>>>>>> e3e57ff9b11e7d3646d23d401e00da3b642fe92c
-
-main(10)
+main(1, 50)
